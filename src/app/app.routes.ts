b56109356caa30aaf73259641a2c0286dbf2ccf1
Gabriel Henrique <gabriel_gyn15@hotmail.com>
2024-06-04 16:03:35 -0300

    feat: login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { CartComponent } from './componentes/cart/cart.component';
 import { ProductdetailsComponent } from './componentes/productdetails/productdetails.component';
-import { DefaultLoginLayoutComponent } from './componentes/default-login-layout/default-login-layout.component';
+import { LoginComponent } from './componentes/pages/login/login.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: DefaultLoginLayoutComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'produtos', component: ProductdetailsComponent },
   { path: 'carrinho', component: CartComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
