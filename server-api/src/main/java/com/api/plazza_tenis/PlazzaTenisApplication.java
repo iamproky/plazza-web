996f62153d4425bd27eb1694f24a3b1f43b6e868
Viniciuspsy <viniciusnb1400@gmail.com>
2024-06-18 16:38:04 -0300

    Criação da API

---
+++ server-api/src/main/java/com/api/plazza_tenis/PlazzaTenisApplication.java
@@ -0,0 +1,13 @@
+package com.api.plazza_tenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazzaTenisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazzaTenisApplication.class, args);
+	}
+
+}
