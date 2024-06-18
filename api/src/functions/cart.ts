26e7c7140e432f02e9297b88e9b30810f43c0162
Pedro Soares <pedrosoares.nex@gmail.com>
2024-06-18 16:39:50 -0300

    Organize folders

--- api/src/functions/cart.ts
+++
@@ -1,9 +0,0 @@
-import Elysia from "elysia";
-
-export const cartRoutes = new Elysia({ prefix: "cart" })
-  .get("/", () => ({
-    name: "product",
-  }))
-  .post("/", () => ({ name: "product" }))
-  .put("/", () => ({ name: "product" }))
-  .delete("/", () => ({ name: "product" }));
--- api/src/functions/products.ts
+++
@@ -1,9 +0,0 @@
-import Elysia from "elysia";
-
-export const productRoutes = new Elysia({ prefix: "product" })
-  .get("/", () => ({
-    name: "product",
-  }))
-  .post("/", () => ({ name: "product" }))
-  .put("/", () => ({ name: "product" }))
-  .delete("/", () => ({ name: "product" }));
